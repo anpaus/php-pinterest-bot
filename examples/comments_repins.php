9cc7041f7863850baf0b3d6e65901f181eb442f6
anpaus <39030124+anpaus@users.noreply.github.com>
2019-11-26 16:17:38 +0100

    Update comments_repins.php

--- examples/comments_repins.php
+++ examples/comments_repins.php
@@ -7,17 +7,19 @@ use seregazhuk\PinterestBot\Factories\PinterestBot;
 $comments = ['Nice!', 'Cool!', 'Very beautiful!', 'Amazing!'];
 
 $bot = PinterestBot::create();
+
 $bot->auth->login( getenv('USER'),  getenv('PASS'));
 
-$board = $bot->boards->info('pawelterlecki', 'ANPAUS');
+echo(getenv('USER'));
+//$board = $bot->boards->info('pawelterlecki', 'ANPAUS');
 
-$pins = $bot->pins->search('cats')->take(1)->toArray();
+//$pins = $bot->pins->search('cats')->take(1)->toArray();
 
-foreach ($pins as $pin) {
+/*foreach ($pins as $pin) {
     // repin to our board
     $bot->pins->repin($pin['id'], $board['id']);
     // write a comment
     $comment = $comments[array_rand($comments)];
     $bot->comments->create($pin['id'], $comment);
     sleep(1);
-}
+}*/
