cf15f17be7f5dc029a18989721e622ab3d3862ae
anpaus <39030124+anpaus@users.noreply.github.com>
2019-11-29 20:55:37 +0100

    Update comments_repins2.php

--- examples/comments_repins2.php
+++ examples/comments_repins2.php
@@ -29,3 +29,29 @@ foreach ($pins as $pin) {
     //$bot->comments->create($pin['id'], $comment);
     sleep(1);
 }
+
+$board = $bot->boards->info('pawelterlecki', 'Twitch Streamer');
+
+$pins = $bot->topics->pins('twitch')->take(5)->toArray();
+//$pins = $bot->pins->feed(5);
+    
+foreach ($pins as $pin) {
+    // repin to our board
+    $bot->pins->repin($pin['id'], $board['id']);
+    // write a comment
+    //$comment = $comments[array_rand($comments)];
+    //print_r($pin['pinner']);
+    //print_r($pin['board']);
+    /*foreach ($pin as $p) {
+        print_r($p);
+        echo("<br>");
+    }*/
+    $bot->pinners->follow($pin['pinner']['username']);
+    $info = $bot->boards->info($pin['pinner']['username'], $pin['board']['name']);
+    $bot->boards->follow($info['id']);
+    $test = $bot->boards->followers($info['id'])->take(1)->toArray();
+    //print_r($test);
+    //$bot->comments->create($pin['id'], $comment);
+    sleep(1);
+}
+
